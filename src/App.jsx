80f0e2983bd0b1991659eeb5e50ac0c4ffac5ad2
Vigneshwaran <vigneshcj001@gmail.com>
2025-11-02 23:18:25 +0530

    LinkedIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api"; // Your FastAPI backend URL
-
+const API_BASE_URL ="http://linkedin-scraper-dashboard-backend.onrender.com/api";
 function App() {
   const [rapidApiKey, setRapidApiKey] = useState("");
   const [activeTab, setActiveTab] = useState("profile");
